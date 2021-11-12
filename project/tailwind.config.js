441fec552a5e963db42e5c54996b73a9ff658c49
Hamed Taherpour <hamedrazor54@gmail.com>
2021-11-12 16:08:33 +0330

    fixed

--- project/tailwind.config.js
+++ project/tailwind.config.js
@@ -1,7 +1,6 @@
 const colors = require('tailwindcss/colors')
 module.exports = {
-  mode: 'jit',
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: ['./src/**/*.html', './src/**/*.vue', './src/**/*.jsx',],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
