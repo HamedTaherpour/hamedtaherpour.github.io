88ef0b7d2a757316aa7d201543a9efd717a3f7fc
Hamed Taherpour <hamedrazor54@gmail.com>
2021-12-24 21:39:42 +0330

    update

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,26 +1,29 @@
 <template>
-  <div class="font-App h-screen w-full bg-white">
-    <div class="container h-full flex flex-col justify-center">
+  <div class="font-App h-screen w-full">
+    <div class="h-full flex flex-col justify-center">
       <div
         id="profile"
         class="
           flex flex-col
-          p-4
+          md:p-4
           md:flex-row
-          items-center
-          justify-center
-          md:justify-around
+          md:items-center
+          md:justify-center
+          md:gap-x-4
         "
       >
         <img
-          src="/public/img/avatar.jpg"
+          src="/img/avatar.jpg"
           title="This is me"
           alt="Hamed Taherpour"
           class="
-            w-56
+            block
+            mx-auto
+            md:mx-px
+            w-52 md:w-56
             h-52
-            rounded-lg
-            shadow-md
+            rounded-full
+            md:rounded-none
             object-cover
             transform
             transition
@@ -28,31 +31,30 @@
             duration-300
           "
         />
-        <div class="mt-5 md:mt-0">
-          <h1 class="text-2xl font-medium font-">
+        <div class="p-4">
+          <h1 class="text-2xl font-medium">
             Hello, My name is Hamed Taherpour.
           </h1>
           <h3 class="text-lg mt-3 tracking-wider">
             I'm a front-end
             <img class="w-5 inline-block" src="/public/img/vue_logo.png" />
             developer at
-            <a href="https://majaziland.me/" class="text-link" target="_blank"
-              >Majaziland</a
-            >
+            <a href="https://majaziland.me" class="text-link" target="_blank">Majaziland</a>
+            &nbsp;
+            <a href="https://arzdigital.com" class="text-link" target="_blank">ArzDigital</a>
             <br />
             I <span class="mr-1 text-rose-500 font-semibold">love ♥</span>
-            <a href="https://tailwindcss.com/" class="text-link" target="_blank"
-              >Tailwindcss</a>
+            <a href="https://tailwindcss.com/" class="text-link" target="_blank">Tailwindcss</a>
           </h3>
           <div class="flex flex-wrap justify-start items-center gap-x-4 mt-5">
             <a
-              class="transform transition hover:rotate-12 hover:scale-110"
+              class="transform transition hover:rotate-12 hover:scale-110 hidden"
               href="https://join.skype.com/invite/iZ84fXhcShAo"
               target="_blank"
             >
               <img
-                class="w-7"
-                src="/public/img/skype.png"
+                class="w-6"
+                src="/img/skype.png"
                 alt="skype"
                 title="skype"
               />
@@ -63,8 +65,8 @@
               target="_blank"
             >
               <img
-                class="w-7"
-                src="/public/img/github.png"
+                class="w-6"
+                src="/img/github.png"
                 alt="github"
                 title="my open source project"
               />
@@ -75,8 +77,8 @@
               target="_blank"
             >
               <img
-                class="w-7"
-                src="/public/img/twitter.png"
+                class="w-6"
+                src="/img/twitter.png"
                 alt="twitter"
                 title="follow me on twitter"
               />
@@ -87,32 +89,32 @@
               target="_blank"
             >
               <img
-                class="w-7"
-                src="/public/img/telegram.png"
+                class="w-6"
+                src="/img/telegram.png"
                 alt="telegram"
                 title="telegram"
               />
             </a>
             <a
-              class="transform transition hover:rotate-12 hover:scale-110"
+              class="transform transition hover:rotate-12 hover:scale-110 hidden"
               href="https://wa.me/09371508754"
               target="_blank"
             >
               <img
-                class="w-7"
-                src="/public/img/whatsapp.png"
+                class="w-6"
+                src="/img/whatsapp.png"
                 alt="whatsapp"
                 title="whatsapp"
               />
             </a>
             <a
-              class="transform transition hover:rotate-12 hover:scale-110"
+              class="transform transition hover:rotate-12 hover:scale-110 hidden"
               href="https://www.instagram.com/hamed.taherpour/"
               target="_blank"
             >
               <img
-                class="w-7"
-                src="/public/img/instagram.png"
+                class="w-6"
+                src="/img/instagram.png"
                 title="follow me on instagram"
                 alt="instagram"
               />
